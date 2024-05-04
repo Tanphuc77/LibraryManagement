47987303a6a69c1395773a7e62d6672365bd6773
PhanTanPhuc <phuccode77@gmail.com>
2024-05-04 09:19:12 +0700

    feat Statistical

--- LibraryManagement/Controllers/StatisticalController.cs
+++ LibraryManagement/Controllers/StatisticalController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace LibraryManagement.Controllers
 {
-    //[Authorize(Roles = "QUANLYSACH")]
+    [Authorize(Roles = "QUANLYSACH")]
     public class StatisticalController : Controller
     {
         QuanLyThuVienEntities db = new QuanLyThuVienEntities();
@@ -71,6 +71,46 @@ namespace LibraryManagement.Controllers
             bookViewModels = bookViewModels.OrderByDescending(x => x.TotalBorrowQuantity).ToList();
 
             return PartialView(bookViewModels);
+        }
+        public ActionResult OutofdateBook()
+        {
+            List<OutofdateBook> outofdateBooks = new List<OutofdateBook>();
+
+            // Kết nối với cơ sở dữ liệu và lấy thông tin cần thiết
+            using (QuanLyThuVienEntities db = new QuanLyThuVienEntities())
+            {
+                // Truy vấn để lấy thông tin về các đọc giả mượn sách quá hạn
+                var query = from muontra in db.MUONTRAs
+                            join chitiet in db.CHITIETMUONTRAs on muontra.MAMUON equals chitiet.MAMUON
+                            join docgia in db.DOCGIAs on muontra.MADOCGIA equals docgia.ID
+                            join lop in db.LOPs on docgia.MALOP equals lop.MALOP
+                            where muontra.HANTRA > DateTime.Now // Lọc ra các đọc giả mượn sách quá hạn
+                            select new OutofdateBook
+                            {
+                                HOTEN = docgia.HOTEN,
+                                TENLOP = lop.TENLOP,
+                                TENSACH = chitiet.TENSACH,
+                                SOLUONGMUON = (int)chitiet.SOLUONGMUON,
+                                NGAYMUON = (DateTime)muontra.NGAYMUON,
+                                HANTRA = (DateTime)muontra.HANTRA
+                            };
+
+                // Thêm kết quả vào danh sách outofdateBooks
+                outofdateBooks = query.ToList();
+            }
+
+            // Trả về view và truyền danh sách outofdateBooks vào view
+            return View(outofdateBooks);
+        }
+        public ActionResult BorrowBooksMax10()
+        {
+            var borrowBook = db.SACHes.OrderByDescending(m => m.SOLUONGMUON).Take(10);
+            return View(borrowBook);
+        }
+        public ActionResult ViewBooksMax10()
+        {
+            var ViewBooks = db.SACHes.OrderByDescending(m => m.LUOTXEM).Take(10);
+            return View(ViewBooks);
         }
 
     }
---
+++ LibraryManagement/Models/OutofdateBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagement.Models
+{
+    public class OutofdateBook
+    {
+        public string HOTEN { get; set; }
+        public string TENLOP { get; set; }
+        public string TENSACH { get; set; }
+        public int SOLUONGMUON { get; set; }
+        public DateTime NGAYMUON { get; set; }
+        public DateTime HANTRA { get; set; }
+
+        // Khởi tạo không tham số
+        public OutofdateBook()
+        {
+        }
+
+        // Khởi tạo với các tham số
+        public OutofdateBook(string hoTen, string tenLop, string tenSach, int soLuongMuon, DateTime ngayMuon, DateTime hanTra)
+        {
+            this.HOTEN = hoTen;
+            this.TENLOP = tenLop;
+            this.TENSACH = tenSach;
+            this.SOLUONGMUON = soLuongMuon;
+            this.NGAYMUON = ngayMuon;
+            this.HANTRA = hanTra;
+        }
+    }
+
+}
